f3537a804944f9e6be168dd166ccf663119c1f4d
Dustin Barker <barkerd85@yahoo.com>
2021-12-13 19:24:01 -0700

    Updated the user login page to go to home page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,48 +3,103 @@ import useState from 'react'
 import axios from 'axios';
 
     
-    const Login = () => {
-        async function send(values) {
-            const response = await axios.post("https://localhost:44394/api/authentication", values);
-            if (response.data.token) {
-                localStorage.setItem('token', response.data.token);
-                window.location.reload();
-            }
-        }
-        return send;
-    }
+//     const Login = () => {
+//         async function send(values) {
+//             const response = await axios.post("https://localhost:44394/api/authentication", values);
+//             if (response.data.token) {
+//                 localStorage.setItem('token', response.data.token);
+//                 window.location.reload();
+//             }
+//         }
+//         return send;
+//     }
 
 
-    const Form = (callback) => {
-        const [formValues, setFormValues] = useState({});
+//     const Form = (callback) => {
+//         const [formValues, setFormValues] = useState({});
     
-        const handleChange = (event) => {
-            event.persist();
-            setFormValues({ ...formValues, [event.target.name]: event.target.value })
-        }
+//         const handleChange = (event) => {
+//             event.persist();
+//             setFormValues({ ...formValues, [event.target.name]: event.target.value })
+//         }
     
-        const handleSubmit = (event) => {
+//         const handleSubmit = (event) => {
+//             event.preventDefault();
+//             callback();
+//         }    
+//         return { formValues, handleChange, handleSubmit };
+
+
+// function Login(){
+
+    export default class Login extends React.Component {
+        state = {
+            userName: '',
+            password: '',
+           
+          }
+            
+        
+          handleChange = event => {
+            this.setState({ 
+                [event.target.name] : event.target.value
+            });
+          }
+              
+        
+          onSubmit = async event => {
             event.preventDefault();
-            callback();
-        }    
-        return { formValues, handleChange, handleSubmit };
+             
+        
+            await axios ({
+                method:"post", 
+                url:"https://localhost:44394/api/authentication/login",
+                data:{          
+                    "userName": this.state.userName,
+                    "password": this.state.password,
+                    
+                    
+                }
+            })
+            window.location="/home"
+          }
+          render(){
+            return (
+                        <div>
+                            <body>
+                            <center><h3>Login</h3></center>
+                                <form onSubmit={this.handleSubmit}>
+                                <center><label for="userName">User Name:</label><input name="userName" type="text" value={this.state.userName} onChange={this.handleChange}></input></center>
+                                <center><label for="password">Password:</label><input name="password" type="password" value={this.state.password} onChange={this.handleChange}></input></center>
+                                <center><button type="submit">Login</button></center>
+                                </form>
+                                </body>
+                        </div>
+                    )
+                }
+                                    
+            }
+                                
 
 
-function Login(){
-
-    return (
-        <div>
-            <div>
-            <center><h3> Please Enter Your Login Information  </h3></center>
-                    <form>
-                    <center><label for="Email">Email Address:</label><input type="text"></input></center>
-                    <center><label for="Password">Password:</label><input type="text"></input></center>
-                    <center><button onSubmit={"loginUser"} className='button1'>Login</button></center>
-                    </form>
-            </div>
-        </div>
-    )}
-}
 
 
-export default Login; 
+
+
+
+
+//     return (
+//         <div>
+//             <div>
+//             <center><h3> Please Enter Your Login Information  </h3></center>
+//                     <form>
+//                     <center><label for="Email">Email Address:</label><input type="text"></input></center>
+//                     <center><label for="Password">Password:</label><input type="text"></input></center>
+//                     <center><button onSubmit={"loginUser"} className='button1'>Login</button></center>
+//                     </form>
+//             </div>
+//         </div>
+//     )}
+// }
+
+
